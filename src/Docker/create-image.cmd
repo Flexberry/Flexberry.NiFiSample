9202176b088aea762e4204041f8063875c3ca58d
Andrey Chibisov <achibisov@skyori.ru>
2023-03-21 15:48:19 +0500

    Remove clickhouse and nifi docker part

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,7 +1,3 @@
 docker build --no-cache -f SQL\Dockerfile.PostgreSql -t nifisample/postgre-sql ../SQL
 
 docker build --no-cache -f Dockerfile -t nifisample/app ../..
-
-docker build --no-cache -f SQL\Dockerfile.ClickHouse -t nifisample/clickhouse ../SQL
-
-docker build --no-cache -f Dockerfile.NiFi -t nifisample/nifi ..
