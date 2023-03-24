e70d5e3839de3e903bedeab9ca243d4dc345ae1d
TeterinaSvetlana <40786685+TeterinaSvetlana@users.noreply.github.com>
2023-03-24 14:08:29 +0500

    Feature 231681 add grafana loki (#10)
    Добавлен сервис NiFi

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -3,3 +3,5 @@ docker build --no-cache -f SQL\Dockerfile.PostgreSql -t nifisample/postgre-sql .
 docker build --no-cache -f Dockerfile -t nifisample/app ../..
 
 docker build --no-cache -f Dockerfile.NiFi -t nifisample/nifi ..
+
+docker build --no-cache -f GrafanaLoki/Dockerfile.Loki -t nifisample/loki .
