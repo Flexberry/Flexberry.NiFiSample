1f7c33cc8194e466c3e1ccba5a9f227e98fd194b
TeterinaSvetlana <40786685+TeterinaSvetlana@users.noreply.github.com>
2023-03-24 10:08:53 +0500

    Добавлен сервис NiFi (#8)
    Добавлен сервис NiFi

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,3 +1,5 @@
 docker build --no-cache -f SQL\Dockerfile.PostgreSql -t nifisample/postgre-sql ../SQL
 
 docker build --no-cache -f Dockerfile -t nifisample/app ../..
+
+docker build --no-cache -f Dockerfile.NiFi -t nifisample/nifi ..
