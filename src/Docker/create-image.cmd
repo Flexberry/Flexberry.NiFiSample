5c8197ce49a2f640ff503beeb681321c68cc529b
Andrey Chibisov <achibisov@skyori.ru>
2023-03-24 14:38:53 +0500

    File system restructuring

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,8 +1,8 @@
-docker build --no-cache -f Dockerfiles/Dockerfile -t nifisample/app ../..
+docker build --no-cache -f Dockerfile -t nifisample/app ../..
 
-docker build --no-cache -f Dockerfiles/Dockerfile.PostgreSql -t nifisample/postgre-sql ../SQL
+docker build --no-cache -f SQL/Dockerfile.PostgreSql -t nifisample/postgre-sql ../SQL
 
-docker build --no-cache -f Dockerfiles/Dockerfile.Audit.Clickhouse -t nifisample/audit-clickhouse ../SQL
+docker build --no-cache -f SQL/Dockerfile.ClickhouseAudit -t nifisample/audit-clickhouse ../SQL
 
 docker build --no-cache -f Dockerfile.NiFi -t nifisample/nifi ..
 
