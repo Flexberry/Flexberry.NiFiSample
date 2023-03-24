ba5de51f6f41966f8822c75f685efaa73f865659
Andrey Chibisov <achibisov@skyori.ru>
2023-03-24 12:21:39 +0500

    Rename audit variables

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -2,6 +2,6 @@ docker build --no-cache -f Dockerfiles/Dockerfile -t nifisample/app ../..
 
 docker build --no-cache -f Dockerfiles/Dockerfile.PostgreSql -t nifisample/postgre-sql ../SQL
 
-docker build --no-cache -f Dockerfiles/Dockerfile.Clickhouse -t nifisample/clickhouse ../SQL
+docker build --no-cache -f Dockerfiles/Dockerfile.Audit.Clickhouse -t nifisample/audit-clickhouse ../SQL
 
 docker build --no-cache -f Dockerfile.NiFi -t nifisample/nifi ..
