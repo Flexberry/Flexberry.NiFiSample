32893a5427529908b41154f306adee6d2a324f9c
Ruslan Bannikov <bannikov.ru@inbox.ru>
2023-03-27 14:14:32 +0500

    add superset: docker images

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -1,11 +1,11 @@
+docker build --no-cache -f SQL\Dockerfile.PostgreSql -t nifisample/postgre-sql ../SQL
+
 docker build --no-cache -f Dockerfile -t nifisample/app ../..
-
-docker build --no-cache -f SQL/Dockerfile.PostgreSql -t nifisample/postgre-sql ../SQL
-
-docker build --no-cache -f SQL/Dockerfile.ClickhouseAudit -t nifisample/audit-clickhouse ../SQL
 
 docker build --no-cache -f Dockerfile.NiFi -t nifisample/nifi ..
 
 docker build --no-cache -f GrafanaLoki/Dockerfile.Loki -t nifisample/loki .
 
 docker build --no-cache -f SQL/Dockerfile.ClickhouseAnalytics -t nifisample/clickhouseanalytics ../SQL
+
+docker build --no-cache -f Superset/Dockerfile.Superset -t nifisample/superset .
