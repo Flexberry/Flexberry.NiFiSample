7b9e46ca8130849a1dc820e8dc494eff3d764567
BannikovRuslan <bannikov.ru@inbox.ru>
2023-03-28 09:00:54 +0500

    add superset (#17)

--- src/Docker/create-image.cmd
+++ src/Docker/create-image.cmd
@@ -9,3 +9,5 @@ docker build --no-cache -f Dockerfile.NiFi -t nifisample/nifi ..
 docker build --no-cache -f GrafanaLoki/Dockerfile.Loki -t nifisample/loki .
 
 docker build --no-cache -f SQL/Dockerfile.ClickhouseAnalytics -t nifisample/clickhouseanalytics ../SQL
+
+docker build --no-cache -f Superset/Dockerfile.Superset -t nifisample/superset .
